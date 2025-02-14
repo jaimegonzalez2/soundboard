7403bfcce4767813cca823d376eef4d71045c92e
jaimegonzalez2 <95113326+jaimegonzalez2@users.noreply.github.com>
2025-02-13 18:47:02 -0600

    Update script.js
    Updated with more sounds

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("JavaScript Loaded!");
     
     const sounds = {
-        sound1: { file: new Audio('sounds/my_leg.mp3'), label:'MyLeg'},
-        sound2: { file: new Audio('sounds/simpsons.mp3'), label: 'InDanger'},
-        sound3: { file: new Audio('sounds/workaholics.mp3'), label: 'Workaholic'}
+        sound1: { file: new Audio('sounds/my_leg.mp3'), label:'My Leg'},
+        sound2: { file: new Audio('sounds/simpsons.mp3'), label: 'In Danger'},
+        sound3: { file: new Audio('sounds/workaholics.mp3'), label: 'Almost Died'},
+        sound4: { file: new Audio('sounds/CHAINSAW.mp3'), label: 'Chainsaw'},
+        sound5: { file: new Audio('sounds/Wilhelm_Scream.mp3'), label: 'Wilhelm Scream'},
+        sound6: { file: new Audio('sounds/the_holy_grail_runaway.mp3'), label: 'Holy Grail'}
+
         // Add additional audio files here in the format below:
         // sound4: new Audio('sounds/sound4.mp3'),
         // sound5: new Audio('sounds/sound5.mp3')
