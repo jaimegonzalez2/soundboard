d4682d909b877e45a2bf6d98bf6d4d91afb7f40d
jaimegonzalez2 <95113326+jaimegonzalez2@users.noreply.github.com>
2025-02-15 11:00:32 -0600

    Update script.js
    Updated labels to buttons.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     
     const sounds = {
         sound1: { file: new Audio('sounds/my_leg.mp3'), label:'My Leg'},
-        sound2: { file: new Audio('sounds/simpsons.mp3'), label: 'In Danger'},
-        sound3: { file: new Audio('sounds/workaholics.mp3'), label: 'Almost Died'},
+        sound2: { file: new Audio('sounds/simpsons.mp3'), label: 'I\'m In Danger'},
+        sound3: { file: new Audio('sounds/workaholics.mp3'), label: 'I Almost Died'},
         sound4: { file: new Audio('sounds/CHAINSAW.mp3'), label: 'Chainsaw'},
         sound5: { file: new Audio('sounds/Wilhelm_Scream.mp3'), label: 'Wilhelm Scream'},
         sound6: { file: new Audio('sounds/the_holy_grail_runaway.mp3'), label: 'Holy Grail Runaway'},
