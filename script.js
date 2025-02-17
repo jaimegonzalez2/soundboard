c7c4ca50b7f5cd09cee852d9f0cb5985819506c5
jaimegonzalez2 <95113326+jaimegonzalez2@users.noreply.github.com>
2025-02-17 11:29:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
         sound21: { file: new Audio('sounds/KABOOM.mp3'), label: 'Explosion'},
         sound22: { file: new Audio('sounds/I_see_dead_people.mp3'), label: 'I See Dead People'},
         sound23: { file: new Audio('sounds/spell_casting.mp3'), label: 'Spell Casting'},
-        sound21: { file: new Audio('sounds/suspicious.mp3'), label: 'Don\'t Be Suspicious'}
+        sound24: { file: new Audio('sounds/suspicious.mp3'), label: 'Don\'t Be Suspicious'}
         // Add additional audio files here in the format below:
         // sound#: new Audio('sounds/sound#.mp3'),
     };
